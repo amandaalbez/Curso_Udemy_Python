a0e4000f8da56ab1a00722f9c305828138a4c800
Amanda Alcântara <104281621+amandaalbez@users.noreply.github.com>
2025-03-07 19:57:27 -0300

    Add string slicing examples in aula27.py

---
+++ secao_3/aula27.py
@@ -0,0 +1,15 @@
+"""
+Fatiamento de strings
+012345678 - Indices Positivos 
+Olá mundo
+-987654321 - Indices Ngeativos
+
+Fatiamento [i:f:p] [::]
+Obs: a função len retorna a quantidade de caracteres da str
+"""
+variavel = 'Olá Mundo!'
+print(variavel[5])#Pegando o indice 5, letra 'u'
+
+print(variavel[4:]) #Se vc não colocar o fim, ele sabe que deve mostrar até o final
+print(variavel[4:7]) #Colocando valor final
+
